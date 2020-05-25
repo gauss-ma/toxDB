ce3d1989aabf0cf56c03c0d6bfcc086b4d0a70f8
ramespada <espada@agro.uba.ar>
2020-05-25 18:44:55 -0300

    Reducccion de todos los scripts a uno: src/buildToxDB.sh; generacion de tabla de compuestos de EPA.

---
+++ src/GSI/README.md
@@ -0,0 +1,51 @@
+# tóxico.db
+
+> base de datos de propiedades fisico-químicas y toxicológicas de compuestos de interés ambiental.
+
+### Referencias:
+
+---
+Variable           | Descripción:                                  |
+--------------     | --------------------------------------------- |
+**`` name     ``** | Name of the compound of interest              |
+**`` CAS      ``** | Chemical Abstracts Service Registry Number    |
+**`` type     ``** | Type (O:Organic; I:Inorganic)                 |
+**`` m_mol    ``** | Molecular Weight (g/mol)		   	   |
+**`` S        ``** | Solubility @ 20-25 degC (mg/L)		   |
+**`` p_vap    ``** | Vapor pressure @ 20-25 degC (mmHG)	           |
+**`` H        ``** | Henrys Law constant @ 20 degC		   |
+**`` Koc      ``** | Sorption coefficient (log L/kg) Koc	   |
+**`` Kow      ``** | Octanol-water partition coefficient (log L/kg)|
+**`` D_air    ``** | Diffusion coefficient in air (cm2/s)	   |
+**`` D_w      ``** | Diffusion coefficient in water (cm2/s)        |
+**`` BA       ``** | Relative bioavailability factor (-)           |
+**`` LoD_w    ``** | Water (mg/L)			           |
+**`` LoD_s    ``** | Soil (mg/kg)			           |
+**``lambda_s  ``** | Saturated zone			           |
+**``lambda_u  ``** | Unsaturated zone			           |
+**``theta_ag  ``** | Above ground veg.			           |
+**``theta_bg  ``** | Below ground veg.			           |
+**``EPA_w     ``** | EPA weight of evidence		           |
+**``is_carc   ``** | Carcinogen				           |
+**``SF_o      ``** | Oral slope factor (1/[mg/kg/day])	           |
+**``UR_inhal  ``** | Inhalation unit risk factor (1/[ug/m3])       |
+**``Ref_oral  ``** | Oral reference dose (mg/kg/day)	           |
+**``Ref_inh   ``** | Inhalation reference conc. (mg/m3)	           |
+**``ads_derm  ``** | Dermal adsorption fraction (-)	           |
+**``ads_gast  ``** | Gastrointestinal adsorption fraction (-)      |
+**``derm_per  ``** | Dermal permeability coefficient (cm/hr)       |
+**``derm_lag  ``** | Lag time for dermal exposure (hr)	           |
+**``derm_exp  ``** | Critical dermal exposure time (hr)	           |
+**``derm_contr``** | Relative contribution of perm. coeff. (-)     |
+**``MCL_1     ``** | Primary CL				           |
+**``MCL_2     ``** | Secondary CL				   |
+**``PEL_TWA   ``** | Occupational Air PEL/TWA (mg/m3)	           |
+**``AqLP_w    ``** | Aquatic life protection: Fresh water biota    |
+**``AqLP_bio  ``** | Aquatic life protection: arine biota	   |
+**``HH_w_drink``** | Human health: Drinking / freshwater fish	   |
+**``HH_fish   ``** | Human health: Fresh water fishing only	   |
+**``HH_fish_salt``** | Human health: Salt water fishing only       |
+
+
+---
+
---
+++ src/buildToxDB.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+ToxDB_out="Tox.db"
+ToxDB_in="Rechazados_ChemIDPlus.lst" #"lista.lst"
+
+IFS=$'\n' CASs=($(awk -F ";" '{print $1}' ${ToxDB_in}))
+IFS=$'\n' nombres=($(awk -F ";" '{print $2}' ${ToxDB_in}))
+
+for i in $(seq 0 ${#CASs[@]})
+do
+	cas=${CASs[i]}
+	nombre=${nombres[i]}
+
+        echo -en "${nombre} \e[31m ${cas} : \e[0m"
+
+	#=>Buscar CIDs:
+        tmp_json=$(curl -s "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/xref/rn/${cas}/cids/JSON")
+	if [ $? -eq 0 ]
+        then
+        	cid=$(echo $tmp_json | jq -r '.IdentifierList.CID[0]')
+		echo -e "\e[32m $cid \e[0m"
+        else
+        	echo -e "\e[31m CID no encontrado.\e[0m"; echo "${cas};${nombre}">> Rechazados_CID.lst
+		continue
+        fi;
+
+	#=>ChemIDPlus: 
+	#FísicoQuímica y Toxicología
+        	tmp_json=$(curl "https://chem.nlm.nih.gov/api/data/rn/sw/${cas}/?data=details")
+		if [ $? -eq 0 ]
+                then
+                	fisicoquimica=$(echo $tmp_json | jq -r '.results[0].physicalProps')
+                	toxicologia=$(  echo $tmp_json | jq -r '.results[0].toxicityList')
+                	#echo -e "\e[35m $fisicoquimica \e[0m"; 	echo -e "\e[35m $toxicologia \e[0m"
+                else
+                	echo -e "\e[31m ChemIDPlus data no encontrado.\e[0m"; echo "${cas};${nombre}" >> Rechazados_ChemIDPlus.lst
+			continue
+                fi;
+
+	#=>PubChem:
+        #Imagen de estructura 2D y 3D:
+		img2D_file=PubChem/img2D/${cid}.png;img3D_file=PubChem/img3D/${cid}.png
+		imgNotAvail_file=PubChem/img3D/NotAvailable.png;imgNoDispon_file=PubChem/img3D/NoDisponible.png
+                wget --quiet -O "${img2D_file}" "https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=${cid}&t=s"
+                wget --quiet -O "${img3D_file}" "https://pubchem.ncbi.nlm.nih.gov/image/img3d.cgi?&cid=${cid}&t=s"
+                #Cambio fondo blanco por transparente y pos-procesamiento:
+                convert ${img2D_file} -transparent "#F5F5F5" ${img2D_file}
+		compare -metric AE ${imgNotAvail_file} ${img3D_file} null
+		if [ $? -eq 0 ]
+		then
+			cp ${imgNoDispon_file} ${img3D_file}
+		else
+			convert ${img3D_file} -transparent white ${img3D_file}
+		fi;
+        
+	#Seguridad Química:
+                tmp_json=$(curl "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/${cid}/JSON?heading=Chemical+Safety")
+                if [ $? -eq 0 ] 
+                then
+			GHSs="$(echo $tmp_json | jq '.. | ."Extra"? |strings' | awk -F '\n' 'BEGIN{printf "["};{printf("%s,", $1)};END{printf "],\n"}')"
+                else
+                	echo -e "\e[31m PubChem Safety data no encontrado.\e[0m"; echo "${cas};${nombre}" >> Rechazados_PubChem_safety.lst
+                fi
+
+	#Print en ToxDB:
+		printf " {\n"						>> $ToxDB_out
+			printf "   \"name\" : \"${nombre}\",\n" 	>> $ToxDB_out
+			printf "   \"CAS\" : \"${cas}\",\n" 		>> $ToxDB_out
+			printf "   \"CID\" : ${cid},\n" 		>> $ToxDB_out
+			printf "   \"PhysProps\": ${fisicoquimica},\n"  >> $ToxDB_out
+			printf "   \"ToxProps\": ${toxicologia},\n" 	>> $ToxDB_out
+			printf "   \"GHS\" : ${GHSs}\n"			>> $ToxDB_out 
+		printf " },\n" 						>> $ToxDB_out
+done;
+	#=>PubChem: Summary
+		#props="MolecularFormula,MolecularWeight,IUPACName,XLogP,CanonicalSMILES,Charge,Fingerprint2D"
+		#curl "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/${cids_str}/property/${props}/JSON"
+
+	#Merge jsons:
+		#jq -s '.[0] * .[1]' file1 file2
+
+
+	# ----
+	#POST-Procesamiento:
+	#Bug en ChemIDPlus, doble "" en cita a bibliografía.
+	printf "TOXDB=[\n" >${ToxDB_out}.js
+	sed "s/\(\"t\": \)\"\"\([^\"]*\)\"/\1\"'\2'/g" $ToxDB_out >> ${ToxDB_out}.js
+	printf "\n]" >>${ToxDB_out}.js
